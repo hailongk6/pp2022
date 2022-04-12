c123199e0346b88eff849cef3c82aee948e7105f
hailongk6 <83690218+hailongk6@users.noreply.github.com>
2022-04-12 16:29:51 +0700

    Add files via upload

--- Lab4/main.py
+++ Lab4/main.py
@@ -1,33 +1,33 @@
-import math
-import numpy as np
-import curse
+import pack.math 
 
-a = math.inputt()
+
+
+
+import numpy as np
+#import cur
 #input student
 Studentlist = []
 print("Please input number of students:")
 stunum = int(input())
 print("Input info of student(Name and  DoB):")
-Studentlistname = a.number("Student", Studentlist, stunum)
+Studentlistname = pack.math.inputt.number("Student", Studentlist, stunum)
 
 #input course
 Courselist = []
 print("Please input number of courses:")
 cournum = int(input())
 print("Input info of course:")
-Courselist = a.number("Course", Courselist, cournum)
+Courselist = a.inputt.number("Course", Courselist, cournum)
 
 #list
-b = math.list()
+
 print("Student's list")
 print("Name")
-print(b.number("Student",Studentlistname))
+print(b.list.number("Student",Studentlistname))
 
 print("Course's list")
-print(b.number("Course",Courselist))
+print(b.list.number("Course",Courselist))
 
 #rounddown 
 mark = float(input())
-mak = module1.roundmark(mark)
-
-
+mak = rounddown.roundmark(mark)
---
+++ Lab4/pack/math.py
@@ -0,0 +1,32 @@
+class inputt:
+    def number(name, listcc, amoun):
+        for i in range(0, amoun):
+            listt = listcc
+            print(name+str(i+1))
+            if (name == "Course"):
+                listt.append("1")
+                Info = input()
+                listt[i] = Info
+            else:
+                listt.append([])
+                for j in range (0, 2):
+                    listt[i].append("1")
+                    Info1 = input()
+                    listt[i][j] = Info1
+        return listt 
+
+
+class list:
+    def number(x,name,listcc):
+        numlit = len(listcc)
+        for i in range (0, numlit): 
+            if(name =="Course"):
+                print(name+str(i+1),":",listcc[i])
+            else:
+                for j in range(0, 2):
+                    print(name+str(i+1),":",listcc[i][j])
+       
+class rounddown:
+    def roundmark(mark1):
+        mark2 = round(mark1, 1)
+        return mark2
